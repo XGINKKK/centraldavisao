da0ca71bac5746c5621c81c9e63beadfb72e7238
Eduardo Santos <skolashard123@gmail.com>
2025-10-07 11:18:23 +0000

    1. Cole este código o mais alto possível no <head> da página: <!-- Googl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,21 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
        <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5NVQ3WXJ');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
@@ -44,6 +59,16 @@ export default function RootLayout({
         {/* End Meta Pixel Code */}
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5NVQ3WXJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
             src="https://www.facebook.com/tr?id=963251781355194&ev=PageView&noscript=1"
